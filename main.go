8ca80cc3b2bc96545e0b6ce6d049569e13ca1cb9
JiajunDou <itgame@gmail.com>
2024-03-12 21:06:33 +0800

    debug

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func Cors() gin.HandlerFunc {
 		if origin != "" {
 			c.Header("Access-Control-Allow-Origin", "http://localhost:9101") // 可将将 * 替换为指定的域名
 			// c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,TalentPool-Language,token")
+			c.Header("Access-Control-Allow-Headers", "*")
 			// c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
 			c.Header("Access-Control-Allow-Credentials", "true")
 		}
